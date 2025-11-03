cde19a344ca71f0ecaa375f1c1ac04840965ed2c
dmytro2258 <dibur06@gmail.com>
2025-11-03 21:21:06 +0100

    add product, single page, few overall changes

--- flux/urls.py
+++ flux/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("cart/", views.cart, name="cart"),
     path("cart/cheackout/", views.cheackout, name="cheackout"),
+    path("product/<int:pk>/", views.product_detail, name="product_detail"),
 ]
