5ef532aa5bbecf2b78115458c669f64120419244
dmytro2258 <dibur06@gmail.com>
2025-12-08 20:13:04 +0100

    update

--- flux/views.py
+++ flux/views.py
@@ -11,10 +11,49 @@ def home(request):
 
 def checkout(request):
     cart = Cart(request)
+    if len(cart) ==0:
+        return redirect('home')
+    
     if request.method == 'POST':
-        print('Form submitted!')
-        print(request.POST)
-        pass
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        mobile = request.POST.get('mobile')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        country = request.POST.get('country')
+        zip_code = request.POST.get('zip_code')
+        note = request.POST.get('note')
+        
+        order = Order.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            email=email,
+            mobile=mobile,
+            address=address,
+            city=city,
+            country=country,
+            zip_code=zip_code,
+            note=note
+        )
+        
+        for item in cart:
+            product = item['product']
+            quantity = int(item['quantity'])
+            price = item['price']
+            
+            OrderItem.objects.create(
+                order = order,
+                product = product,
+                price = price,
+                quantity = quantity,
+            )
+        
+        cart.clear()
+        
+        return render(request, 'flux/success.html')
+    
+    
     context = {
         "cart" :cart
     }
