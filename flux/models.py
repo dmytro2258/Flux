77f231c39ad00daf4537830091a96029d8271636
dmytro2258 <dibur06@gmail.com>
2026-02-18 09:39:58 +0100

    Basic commit

--- flux/models.py
+++ flux/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    full_description = models.TextField(blank=True, null=True)
+    full_description = models.TextField(blank=True, null=True)  
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
